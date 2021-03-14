74dd01b52a3440bfe966914e531a03c09de77078
Mohinder <63348263+MScodi@users.noreply.github.com>
2021-03-15 01:57:36 +0530

    Create 2-d Array_ pass_in_function.cpp

---
+++ Arrays/2-d Array_ pass_in_function.cpp
@@ -0,0 +1,12 @@
+void twoDimensional(int **arr,int N){
+    
+    // Loop to iterate through matrix
+    for(int i = 0;i<N;i++){
+        for(int j = 0;j<N;j++){
+            
+        cout<<arr[i][j]<<" ";
+        }
+        cout << endl;
+    }
+    
+}
