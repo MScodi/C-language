e462ab3276ba1cf9df5eb869ec54457514061a89
Mohinder <63348263+MScodi@users.noreply.github.com>
2021-03-15 01:59:18 +0530

    Update Swap_By_Reference .cpp

--- C++ OOPS/Functions/Swap_By_Reference .cpp
+++ C++ OOPS/Functions/Swap_By_Reference .cpp
@@ -1,1 +1,19 @@
+// Function to swap A and B
+// using pointers
+void swap(int *A, int *B){
+    
+   int temp;
+   temp=*A;
+   *A=*B;
+   *B=temp;
+    
+}
 
+// Function to swap A and B
+// using reference
+void swap(int& x, int& y) 
+{ 
+    int z = x; 
+    x = y; 
+    y = z; 
+} 
